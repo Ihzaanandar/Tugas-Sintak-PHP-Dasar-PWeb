fc9631819191a5fbdf22806a0cce2d1772a290ea
Ihzaanandar <149031938+Ihzaanandar@users.noreply.github.com>
2024-06-01 17:41:21 +0700

    Add files via upload

---
+++ PWeb2024-Tugas_09__2300018284_Ihza_Ananda_Rachman.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $gaji = 20000000;
+        $pajak = 0.1;
+        $thp = $gaji - ($gaji * $pajak);
+
+        echo ("Program 1 <br>");
+        echo ("Gaji sebelum pajak : Rp. $gaji <br>");
+        echo ("Gaji sesudah pajak : Rp. $thp <br>");
+
+        echo ("<br><br>");
+
+        echo ("Program 2 <br>");
+        $a = 9;
+        $b = 2;
+        echo "$a == $b : " . (($a == $b) ? 'True' : 'False');
+        echo "<br>";
+        echo "$a != $b : " . (($a != $b) ? 'True' : 'False');
+        echo "<br>";
+        echo "$a > $b : " . (($a > $b) ? 'True' : 'False');
+        echo "<br>";
+        echo "$a < $b : " . (($a < $b) ? 'True' : 'False');
+        echo "<br>";
+        echo "($a == $b) && ($a > $b) : " . (($a == $b) && ($a > $b) ? 'True' : 'False');
+        echo "<br>";
+        echo "($a == $b) || ($a > $b) : " . (($a == $b) || ($a > $b) ? 'True' : 'False');
+        ?>
+    
+</body>
+</html>
+<!-- // echo "Menggunakan var_dump <br>"; 
+// echo "$a != $b : "; var_dump($a != $b);
+// echo "Menggunakan var_dump <br>";
+// echo "$a == $b : "; var_dump($a == $b); -->
